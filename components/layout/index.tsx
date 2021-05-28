42290efe1c6da35af854f56f9f17c41edf120c5b
Josué Sandoval <josuesandoval.dev@yahoo.com>
2021-05-28 14:02:33 -0500

    add header

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from './header';
 
 export default function Layout({children}: {children: React.ReactNode}) {
     return (
         <div>
+            <Header />
             {children}
         </div>
     )

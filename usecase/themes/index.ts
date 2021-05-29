7925630e13d10bbca48c9898ae57fe17640141e9
Josué Sandoval <josuesandoval.dev@yahoo.com>
2021-05-29 01:03:11 -0500

    add dark mode

--- usecase/themes/index.ts
+++ usecase/themes/index.ts
@@ -2,17 +2,22 @@ import { ITheme } from '../../domain/model/theme'
 
 import themeBold from '../../styles/theme/bold.module.css'
 import themeLight from '../../styles/theme/light.module.css'
+import themeDark from '../../styles/theme/dark.module.css'
 
 export type TThemeSetter = React.Dispatch<React.SetStateAction<ITheme>>
 
 const themes: Array<ITheme> = [
     {
-        name: 'bold',
-        value: themeBold.theme,
+        name: 'dark',
+        value: themeDark.theme,
     },
     {
         name: 'light',
         value: themeLight.theme,
+    },
+    {
+        name: 'bold',
+        value: themeBold.theme,
     },
 ]
 

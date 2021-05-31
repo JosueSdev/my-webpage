f7a013dba49e46542f3cda94a32f1e710342225f
Josué Sandoval <josuesandoval.dev@yahoo.com>
2021-05-31 12:18:06 -0500

    save theme in local storage

--- components/theme/index.tsx
+++ components/theme/index.tsx
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright © 2021 Josué Ulises Sandoval Jiménez
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { ITheme } from '../../domain/model/theme'
 
-import themes, {TThemeSetter} from '../../usecase/themes'
+import themes, { TThemeSetter, getThemeByName, storageKey } from '../../usecase/themes'
 
 export interface Props {
     children: (theme: ITheme,renderer: TThemeSetter) => React.ReactNode,
@@ -13,9 +13,32 @@ export interface Props {
 
 export default function Theme({children}: Props) {
     const [theme, setTheme] = useState<ITheme>(themes[0])
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const tName = localStorage.getItem(storageKey)
+            if (tName) {
+                const t = getThemeByName(tName)
+
+                if (typeof t === 'undefined') {
+                    return
+                }
+
+                setTheme(t)
+            }
+        }
+    })
+
+    const setThemeHandler: TThemeSetter = (t: ITheme) => {
+        if (typeof window !== 'undefined') {
+            localStorage.setItem(storageKey, t.name)
+        }
+        setTheme(t)
+    }
+
     return (
         <div className={theme.value}>
-            {children(theme, setTheme)}
+            {children(theme, setThemeHandler)}
         </div>
     )
 }
--- usecase/themes/index.ts
+++ usecase/themes/index.ts
@@ -7,7 +7,7 @@ import themeBold from '../../styles/theme/bold.module.css'
 import themeLight from '../../styles/theme/light.module.css'
 import themeDark from '../../styles/theme/dark.module.css'
 
-export type TThemeSetter = React.Dispatch<React.SetStateAction<ITheme>>
+export type TThemeSetter = (T: ITheme) => void
 
 const themes: Array<ITheme> = [
     {
@@ -25,6 +25,8 @@ const themes: Array<ITheme> = [
 ]
 
 export default themes
+
+export const storageKey = 'THEME'
 
 export function getThemeByName(name: string) {
     return themes.find(theme => theme.name == name)

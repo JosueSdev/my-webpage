f22c1b262b4d4a17483efb2d8e1b19b5e1964576
Josué Sandoval <josuesandoval.dev@yahoo.com>
2021-06-03 13:48:29 -0500

    + title to Info view

--- components/views/info/index.tsx
+++ components/views/info/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Head from 'next/head'
 
 import { ILanguage } from '../../../domain/model/language'
 
+import languages from '../../../usecase/languages'
+
 import Layout from '../../layout'
 
+import staticStrings from './strings.json'
 import styles from './styles.module.css'
+
+interface StaticStrings {
+    title: string,
+}
 
 export interface Props {
     children: React.ReactNode,
@@ -15,10 +23,20 @@ export default function Info({
     language,
     children,
 }: Props) {
+    let sStrings: StaticStrings = {} as StaticStrings
+    if (language.id == languages.es.id) {
+        sStrings = staticStrings.es
+    }
+    if (language.id == languages.en.id) {
+        sStrings = staticStrings.en
+    }
     return (
         <Layout
             language={language}
         >
+            <Head>
+                <title>{sStrings.title}</title>
+            </Head>
             <article
                 className={styles.article}
             >

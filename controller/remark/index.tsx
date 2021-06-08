666cf5cbcb58ac0b29b80cde8e2ae842d028e67c
Josué Sandoval <josuesandoval.dev@yahoo.com>
2021-06-07 21:22:41 -0500

    allow markdown with html

--- controller/remark/index.tsx
+++ controller/remark/index.tsx
@@ -5,6 +5,8 @@ import React from 'react'
 import unified from 'unified'
 import markdown from 'remark-parse'
 import remark2rehype from 'remark-rehype'
+import raw from 'rehype-raw'
+import sanitize from 'rehype-sanitize'
 import rehype2react from 'rehype-react'
 
 import CustomLink from './customLink'
@@ -12,7 +14,9 @@ import CustomLink from './customLink'
 export function markdownString2React(md: string) {
     return unified()
         .use(markdown)
-        .use(remark2rehype)
+        .use(remark2rehype, {allowDangerousHtml: true})
+        .use(raw)
+        .use(sanitize)
         .use(rehype2react, {
             createElement: React.createElement,
             components: {
